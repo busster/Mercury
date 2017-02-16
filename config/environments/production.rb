81eecd2720acf028ae84f4bd4b3d2348e0ad504b
busster <jasonmbuss@gmail.com>
2017-02-16 16:41:55 -0600

    remove paperclip stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,18 +94,18 @@ Rails.application.configure do
   #   password: ENV["GMAIL_PASSWORD"]
   # }
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
-  }
-  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-  Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id_partition/:style/:filename"
-  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+  # config.paperclip_defaults = {
+  #   storage: :s3,
+  #   s3_credentials: {
+  #     bucket: ENV.fetch('S3_BUCKET_NAME'),
+  #     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+  #     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+  #     s3_region: ENV.fetch('AWS_REGION'),
+  #   }
+  # }
+  # Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  # Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id_partition/:style/:filename"
+  # Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 
 
   
